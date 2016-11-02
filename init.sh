27bb87d099dfd04244686ecdb283f524f05f9e25
宋庆波 <qingbo.song@gmail.com>
2016-11-02 18:25:59 +0800

    更新初始化脚本

--- init.sh
+++ init.sh
@@ -8,9 +8,16 @@
 #Version:v1.0
 #######################################################
 
+#依赖环境：
+#mplayer   apt-get install mplayer
+#qiv  apt-get install qiv
+#python3  apt-get install python3
+#supervisor  pip install supervisor
+
 #设置开机启动幻灯片
 cp ./supervisord.conf /etc/supervisord.conf
 echo "supervisord -f /etc/supervisord.conf" >> /etc/rc.local ##注意将rc.local下面的exit 0去掉或注释掉
 
 #启动supervisord程序
-supervisord -f /etc/supervisord.conf
+mkidr -p /var/log/supervisor/
+sudo supervisord -c /etc/supervisord.conf
