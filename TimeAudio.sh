aeb7fc7eeccd731ce20fa8d442f38002b35c2683
宋庆波 <qingbo.song@gmail.com>
2016-11-02 15:24:34 +0800

    优化部分命令

--- TimeAudio.sh
+++ TimeAudio.sh
@@ -5,13 +5,13 @@
 #Comment:实现整点报时功能
 #crontab -e : 0 * * * * sudo sh /home/RaspberryPiMTV/TimeAudio.sh 2>&1 >> /tmp/TimeAudio.log
 #E-mail:qingbo.song@gmail.com
-#github:https://github.com/swht
+#github:https://github.com/swht/RaspberryPiMTV.git
 #Version:v1.0
 #######################################################
 
 mp3_path=/home/RaspberryPiMTV/TimeAudio
 video_path=/home/RaspberryPiMTV/TimeVideo
-mplayer_path=/usr/bin/mplayer
+mplayer_path=`which mplayer`
 hour=`date|awk -F " " '{print $5}'|awk -F ":" '{print $1}'`
 hourAudioFile="${mp3_path}/${hour}.mp3"
 hourVideoFile="${video_path}/${hour}.*"
